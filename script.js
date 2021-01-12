a961ae22d30b6790515463a60b8d6de000389273
Fran Carre <francarre19@gmail.com>
2021-01-13 01:02:32 +0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+// FAQS
+// Fetch all details element
+const details = Array.from(document.querySelectorAll("details"));
+
+// Add onclick listeners
+details.forEach(targetDetail => {
+  targetDetail.addEventListener("click", () => {
+    // Close all details that are not targetDetail
+    details.forEach(detail => {
+      if (detail !== targetDetail) {
+        detail.removeAttribute("open");
+      }
+    });
+  });
+});
+
+
+// Initialize AOS
+AOS.init();
